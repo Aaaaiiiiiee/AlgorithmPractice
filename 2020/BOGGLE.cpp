7b2d3dd3c86a53444914332202d2abaa11854367
Aaaaiiiiiee <wjjgjk@naver.com>
2020-01-30 10:25:23 +0900

    Push BOGGLE.cpp

--- 2020/BOGGLE.cpp
+++ 2020/BOGGLE.cpp
@@ -1,5 +1,5 @@
 /*
-*	https://www.algospot.com/judge/problem/read/FESTIVAL
+*	https://www.algospot.com/judge/problem/read/BOGGLE
 *	책 "알고리즘 문제 해결 전략 1" / 구종만 저 / 인사이트 / p.150
 
 *	Aaaaiiiiiee
---
+++ 2020/PICNIC.cpp
@@ -0,0 +1,6 @@
+/*
+*	https://www.algospot.com/judge/problem/read/PICNIC
+*	책 "알고리즘 문제 해결 전략 1" / 구종만 저 / 인사이트 / p.150
+
+*	Aaaaiiiiiee
+*/
